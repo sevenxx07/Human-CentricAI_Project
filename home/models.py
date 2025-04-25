9e956b423bb963799020729fa9d6b5d331c47038
Veronika <veronika.i.anokhina@gmail.com>
2025-04-25 11:54:25 +0200

    Correct typo in models.py; home page url change

--- home/models.py
+++ home/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
-class Person(models.Model):
-    name = models.CharField(max_lenght = 50)
-    matriculation_nr = models.CharField(max_lenght = 30)
+class Student(models.Model):
+    name = models.CharField(max_length=50)
+    matriculation_nr = models.CharField(max_length=30)
+
 
 class Project(models.Model):
-    project_name = models.Charfield(max_lenght = 50)
-    project_URL = models.CharField(max_lenght = 100)
-
-
+    project_name = models.CharField(max_length=50)
+    project_URL = models.CharField(max_length=100)
--- pbl/urls.py
+++ pbl/urls.py
@@ -21,7 +21,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path("home/", include("home.urls")),
+    path("", include("home.urls")),
     path("admin/", admin.site.urls),
     path("demos/", include("demos.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
