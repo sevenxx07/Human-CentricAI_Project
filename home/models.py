284e148cde5bcfa14fd1d2bb2c376fa250b3cf29
stinaaahellgren <stina.hellgren.0180@student.uu.se>
2025-04-28 10:36:09 +0200

    - Created the app Project 1 - Added project 1 to INSTALLED apps in the pbl settings. - Created a urls.py file for project 1. - Added a first view in project1. - Connected the app to the main URL config. - Added project 1 to the project list in HOME views. - Changes the URL for the home page to not get 404 error when visiting the page.

--- home/models.py
+++ home/models.py
@@ -3,10 +3,11 @@ from django.db import models
 
 # Create your models here.
 class Student(models.Model):
-    name = models.CharField(max_length=50)
-    matriculation_nr = models.CharField(max_length=30)
-
+    name = models.CharField(max_lenght = 50)
+    matriculation_nr = models.CharField(max_lenght = 30)
 
 class Project(models.Model):
-    project_name = models.CharField(max_length=50)
-    project_URL = models.CharField(max_length=100)
+    project_name = models.Charfield(max_lenght = 50)
+    project_URL = models.CharField(max_lenght = 100)
+
+
