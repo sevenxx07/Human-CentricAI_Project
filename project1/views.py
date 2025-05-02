f18968e72be3f36496f6f40288fe43e8ccd057bb
stinaaahellgren <stina.hellgren.0180@student.uu.se>
2025-05-02 09:57:06 +0200

    Deleted the duplicate folder project1

--- project1/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-def index(request):
-    return HttpResponse("Welcome to Project 1 - Supervised Learning Interface!")
