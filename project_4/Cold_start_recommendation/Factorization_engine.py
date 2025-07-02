c17918af31c4c33d9a69a3146d84e912f33fdca1
Stinaaahellgren <stina.hellgren.0180@student.uu.se>
2025-07-02 14:40:38 +0200

    Factorization engine with cross-validation of parameters

--- project_4/Cold_start_recommendation/Factorization_engine.py
+++ project_4/Cold_start_recommendation/Factorization_engine.py
@@ -7,23 +7,21 @@ class Matrix_Factorization():
     def __init__(self, matrix):
         self.matrix = matrix
 
-    def factorize(self, K, steps=100, alpha=0.01, lambd=0.1): 
+    def factorize(self, K, steps=10, alpha=0.01, lambd=0.1): 
+        self.alpha = alpha
+        self.K = K
+        self.lambd = lambd
+        self.steps = steps 
         nr_users, nr_items = self.matrix.shape 
+        
         print(f"Number of users:", nr_users)
         print(f"Number of items:", nr_items)
 
-        # Initializing latent matrices
-        # self.U = np.random.normal(scale=1./K, size = (nr_users,K))
-        # self.V = np.random.normal(scale=1./K, size = (nr_items,K))
-
+        # Latent matrices
         self.U = np.random.rand(nr_users,K)
         self.V = np.random.rand(nr_items, K)
 
-        #print("Initial latent matrix U:", U)
-        #print("Initial latent matrix V:", V)
-
-        # Get all ratings that are not NaN
-        self.pairs = list(zip(*np.where(~np.isnan(R.values))))
+        self.pairs = list(zip(*np.where(~np.isnan(self.matrix.values))))
 
         for step in range(self.steps):
             print("Step:", step)
@@ -37,53 +35,52 @@ class Matrix_Factorization():
 
         return self.U, self.V
     
-    def evaluate_loss(self):
-        total_loss = np.inf
-        for i,j in self.pairs: 
+    def evaluate_loss(self, validation_matrix):
+        total_loss = 0
+        validation_pairs = list(zip(*np.where(~np.isnan(validation_matrix.values))))
+        for i,j in validation_pairs: 
             prediction = np.dot(self.U[i,:], self.V[j,:])
-            error = R.iloc[i,j] - prediction
+            error = validation_matrix.iloc[i,j] - prediction
             total_loss += error**2
         total_loss += self.lambd * (np.linalg.norm(self.U)**2 + np.linalg.norm(self.V)**2)
-        if self.step % 10 == 0: 
-            print(f"Step: {self.step}, Loss: {total_loss:.4f}")
+        if self.steps % 10 == 0: 
+            print(f"Step: {self.steps}, Loss: {total_loss:.4f}")
+        return total_loss
     
-    def split_data(self, ratio):
-        nr_users, nr_items = self.matrix.shape 
+    def cross_val(self, alpha_list: list, lambd_list: list, K_list: list, ratio=0.8):
+        nr_users = self.matrix.shape[0]
         split_index = int(nr_users * ratio)
-        matrix_train = self.matrix[:split_index]
-        matrix_validation = self.matrix[split_index:]
-        return matrix_train, matrix_validation 
-    
-    def cross_val(self, alpha_list: list, lambd_list: list, K_list: list, ratio):
-        nr_users, nr_items = self.matrix.shape 
-        split_index = int(nr_users * ratio)
-        matrix_train = self.matrix[:split_index]
-        matrix_validation = self.matrix[split_index:]
-    
+        train_matrix = self.matrix.iloc[:split_index,:]
+        validation_matrix = self.matrix.iloc[split_index:,:]
+
+        best_loss = float('inf')
+        best_parameters = {}
         for a in alpha_list: 
             for l in lambd_list:
                 for k in K_list:
-                    matrix_train.factorize(K=k, alpha=a, lambd=l)
+                    model = Matrix_Factorization(train_matrix)
+                    model.factorize(K=k, alpha=a, lambd=l)
+                    loss = model.evaluate_loss(validation_matrix)
+                    print(f"Alpha={a}, Lambda = {l}, k={k}, loss= {loss:.4f}")
+                    if loss < best_loss:
+                        best_loss = loss
+                        best_parameters = {"alpha": a,"lambda": l, "K": k}
+        print("\n Best Parameters:", best_parameters)
+        return best_parameters
+
 
 
 # Getting the R matrix 
 current_dir = os.path.dirname(__file__)
 data_path = os.path.join(current_dir, 'R_matrix.csv')
-PD = pd.read_csv(data_path, index_col=0)
-alphas = [0.001, 0.005, 0.01, 0.05, 0.1]
-lambdas = [0.01, 0.1, 1]
-Ks = [11, 21, 51, 101]
 
-R = Matrix_Factorization(PD)
-#R.cross_val(alphas, lambdas, Ks, 0.8 )
+R_matrix = pd.read_csv(data_path, index_col=0)
 
+alphas = [0.001, 0.005, 0.01]
+lambdas = [0.01, 0.1, 1.0]
+Ks = [11, 21, 51]
 
 
-alpha = [0.001, 0.005, 0.01, 0.05, 0.1]
-lambdas = [0.01, 0.1, 1]
-K = [11, 21, 51, 101]
+mat_fac_model = Matrix_Factorization(R_matrix)
+best_params = mat_fac_model.cross_val(alphas, lambdas, Ks, ratio=0.8)
 
-# for a in alpha: 
-#     for l in lambdas:
-#         for k in K: 
-#             print(matrix_factorization(R, K, steps = 101))
