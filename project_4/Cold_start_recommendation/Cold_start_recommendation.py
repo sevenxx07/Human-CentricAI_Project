0a43c27d8b9075ae47c93f97cc4924b4704467b9
Stinaaahellgren <stina.hellgren.0180@student.uu.se>
2025-08-06 17:06:50 +0200

    (Not done). Working on predicting the movies to show for intermediate ratings + the final one

--- project_4/Cold_start_recommendation/Cold_start_recommendation.py
+++ project_4/Cold_start_recommendation/Cold_start_recommendation.py
@@ -8,75 +8,126 @@ import json
 import numpy as np
 import random
 
-# ----------1. Cold start ----------#
+# ---------- Cold start ---------- #
 # Learning the initial preference vector U_i based on the 3 initial rated movies 
 
 # TODO: Integrate this into the interface to get the actual ratings of the user. 
+
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+
+csv_path_movies = os.path.join(BASE_DIR, 'data', 'ml_latest_small', 'movies.csv')
+csv_path_ratings = os.path.join(BASE_DIR, 'data', 'ml_latest_small', 'ratings.csv')
+df_movies = pd.read_csv(csv_path_movies)
+df_ratings = pd.read_csv(csv_path_ratings)
+movieId_to_title = dict(zip(df_movies['movieId'], df_movies['title']))
+
+
+
 def get_initial_ratings():
-    selected_movies = get_selected_cold_start_movies()
-    initial_movies = random.sample(selected_movies,3)
+    selected_movies = get_selected_cold_start_movies() 
+    initial_movies = random.sample(selected_movies, 3) 
+    print(f"Initial movies:", initial_movies)
+
+    # Only temporary. Just for testing:
     user_rating = {}
-    for i in range(3):
+    for movie in initial_movies:
         random_rating = random.choice([1,2,3,4,5])
-        user_rating[initial_movies[i]['movieId']] = random_rating 
+        user_rating[movie['movieId']] = random_rating 
+        print("Movie title:", movie['title'], "Movie_ID:", movie['movieId'])
     return user_rating 
 
+#print("Initial rating:", get_initial_ratings())
+
 class ColdStart:
-    def __init__(self, V_matrix, R_matrix, movie_to_index, lambd=0.1):
+    def __init__(self, V_matrix, R_matrix, lambd=0.1):
         self.V = V_matrix
         self.K = V_matrix.shape[1]
         self.R = R_matrix
-        self.movie_to_index = movie_to_index
+        self.R.columns = self.R.columns.astype(int)
         self.lambd = lambd
-
+     
     def update_user_vector(self, user_ratings):
         rated_ids = list(user_ratings.keys())
-        ratings = np.array(list(user_ratings.values()), dtype=np.float64)
-
-        indices = [self.movie_to_index[ids] for ids in rated_ids if ids in self.movie_to_index]
-        if not indices:
-            print("No matching movie IDs found in `movie_to_index`.")
-            print("Rated movie IDs:", rated_ids)
-            print("Available movie IDs:", list(self.movie_to_index.keys())[:10])
-            return np.zeros(self.K)
         
-        V_rated = self.V[indices,:]
+        print("Rated ids:", rated_ids)
+        
+        found_ids = [i for i in rated_ids if i in self.R.columns]
+        ratings = np.array(list(user_ratings.values()), dtype=np.float64)
+        
+        print("Found ids:", found_ids)
 
+        if found_ids != rated_ids:
+            raise ValueError("All rated ids were not found. The found ones were:", found_ids)
+
+        rated_indices = [self.R.columns.get_loc(i) for i in rated_ids if i in self.R.columns]
+
+        V_rated = self.V[rated_indices,:]
+
+        # if len(rated_indices) == 0:
+        #     raise ValueError("None of the rated movie IDS were found")
+        # Learning a new user vector U_i (eq.2 from instructions)
         A = V_rated.T @ V_rated + self.lambd * np.eye(self.K)
         b = V_rated.T @ ratings
         U_i = np.linalg.solve(A,b)
         return U_i
 
-# ---------- 2. Active learning -------------
-# a) Predicting ratings for all unrated movies using the current U
-# b) Select the next movie to display to the user
-# c) Update the user vector after they've rated the next movie
-# d) Explaining how the user's choice will affect the next movie
-
-def active_learning_loop(initial_user_vector, V, movie_to_index, index_to_movie, user_ratings, max_rounds = 3):
+# In the active learning loop, a prediction is made forn unrated ,ovies and the 
+# top predicted movie is selected. The user is asked to rate it, and the vector is 
+# once again updated. 
+def active_learning_loop(initial_user_vector, V, R, user_ratings,  max_rounds = 3):
     U = initial_user_vector
     rated_ids = set(user_ratings.keys())
 
     for i in range(max_rounds):
-        predicted_ratings = V @ U
+        predicted_ratings = V @ U # Predicted ratings for all movies 
         
-        unrated_indices = [i for i in range(V.shape[0]) if index_to_movie[i] not in rated_ids]
-        top_index = max(unrated_indices, key=lambda i:predicted_ratings[i])
-        top_movie_id = index_to_movie[top_index]
-        #top_movie_title = get_movie_title(top_movie_id)
-pass 
+        predicted_ratings = np.clip(predicted_ratings, 0, 5)
+
+        R.columns = R.columns.astype(int)
+        unrated_indices = [idx for idx, movie_id in enumerate(R.columns) if movie_id not in rated_ids] #movies not yet rated by the user
+
+       # Selecting the movie with the highest predicted rating 
+        top_index = max(unrated_indices, key=lambda i:predicted_ratings[i]) 
+        top_movie_id = R.columns[top_index] 
+        top_movie_title = movieId_to_title.get(top_movie_id, "Unknown title")
+
+        print(f" Round {i+1}: Recommended movie: '{top_movie_title}' (movieID: {top_movie_id}) with predicted rating: {predicted_ratings[top_index]:.3f}")
+
+        # TODO: Replace this by the real interface-suitable code. 
+        simulated_rating = random.choice([1,2,3,4,5])
+        user_ratings[top_movie_id] = simulated_rating
+        
+        rated_ids.add(top_movie_id) # Marking the movie as rated 
+
+        cold_start = ColdStart(V,R) # Update the user vector 
+        U = cold_start.update_user_vector(user_ratings)
+
+    
+    return U
 
 
-# ---------- 3. Recommendation  -------------
-# a) Recompute the final user vector
-# b) Recommend the highest predicted rating not yet rated 
+def explain_impact(current_user_ratings, movie_to_rate_id, V, R, movieId_to_title, top_k=1):
+
+    explainations = {}
+    for rating in range(1,6):
+        pass
+
 
 model, R, U, V = get_R_U_V()
+#print("Movie IDS:",movie_ids)
+# print("R shape:", R.shape)
+# print("V shape:", V.shape)
+# print("U shape:", U.shape)
 
-movie_ids = [int(col) for col in R.columns]
-movie_to_index = {ids: index for index, ids in enumerate(movie_ids)}
+# print(type(R))               
+# print(R.columns[:20])       
+# print(R.columns.dtype) 
+
+
 
 user_ratings = get_initial_ratings()
-recommender = ColdStart(V, R, movie_to_index)
-user_vector = recommender.update_user_vector(user_ratings)
-print(user_vector)
+cold_start_recommender = ColdStart(V, R)
+user_vector = cold_start_recommender.update_user_vector(user_ratings)
+final_user_vector = active_learning_loop(user_vector, V, R, user_ratings, max_rounds=3)
+
+print(f"Final user vector:", final_user_vector)
