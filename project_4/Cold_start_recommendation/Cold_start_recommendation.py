cac1d16647b76ff6bf763fae6ac670df3d4d0a54
Stinaaahellgren <stina.hellgren.0180@student.uu.se>
2025-08-08 11:27:03 +0200

    Movie feature interpretations + working on the cold start recommendations

--- project_4/Cold_start_recommendation/Cold_start_recommendation.py
+++ project_4/Cold_start_recommendation/Cold_start_recommendation.py
@@ -1,28 +1,25 @@
-from Clustering import run_true_hybrid_cold_start
 from Clustering import get_selected_cold_start_movies
-from Factorization_engine import Matrix_Factorization
 from Factorization_engine import get_R_U_V 
+from feature_interpretations import feature_dict, feature_characteristics
 import pandas as pd
 import os
-import json
 import numpy as np
 import random
+from numpy.linalg import norm
+
+# Importing df_movies to create a conversion dictironay between movie ID and title
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+csv_path_movies = os.path.join(BASE_DIR, 'data', 'ml_latest_small', 'movies.csv')
+df_movies = pd.read_csv(csv_path_movies)
+movieId_to_title = dict(zip(df_movies['movieId'], df_movies['title']))
+
 
 # ---------- Cold start ---------- #
 # Learning the initial preference vector U_i based on the 3 initial rated movies 
 
 # TODO: Integrate this into the interface to get the actual ratings of the user. 
 
-BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
-
-csv_path_movies = os.path.join(BASE_DIR, 'data', 'ml_latest_small', 'movies.csv')
-csv_path_ratings = os.path.join(BASE_DIR, 'data', 'ml_latest_small', 'ratings.csv')
-df_movies = pd.read_csv(csv_path_movies)
-df_ratings = pd.read_csv(csv_path_ratings)
-movieId_to_title = dict(zip(df_movies['movieId'], df_movies['title']))
-
-
-
+# Obtaining the initial ratings on 3 randomly selected movies (one from each cluster to ensure diversity and informativity).
 def get_initial_ratings():
     selected_movies = get_selected_cold_start_movies() 
     initial_movies = random.sample(selected_movies, 3) 
@@ -36,48 +33,85 @@ def get_initial_ratings():
         print("Movie title:", movie['title'], "Movie_ID:", movie['movieId'])
     return user_rating 
 
-#print("Initial rating:", get_initial_ratings())
 
+# Learning the initial user vector (after the first 3 movies have been rated)
 class ColdStart:
     def __init__(self, V_matrix, R_matrix, lambd=0.1):
-        self.V = V_matrix
+        self.V = V_matrix # Latent ratings matrix
         self.K = V_matrix.shape[1]
-        self.R = R_matrix
+        self.R = R_matrix # Original matrix containing each rating for each user
         self.R.columns = self.R.columns.astype(int)
         self.lambd = lambd
      
     def update_user_vector(self, user_ratings):
+        # Extracting the rated movies and ensuring they are found in the R-matrix (i.e. have some rating from before)
         rated_ids = list(user_ratings.keys())
-        
-        print("Rated ids:", rated_ids)
-        
         found_ids = [i for i in rated_ids if i in self.R.columns]
-        ratings = np.array(list(user_ratings.values()), dtype=np.float64)
-        
-        print("Found ids:", found_ids)
-
         if found_ids != rated_ids:
             raise ValueError("All rated ids were not found. The found ones were:", found_ids)
-
+        
+        ratings = np.array(list(user_ratings.values()), dtype=np.float64)
         rated_indices = [self.R.columns.get_loc(i) for i in rated_ids if i in self.R.columns]
-
         V_rated = self.V[rated_indices,:]
 
-        # if len(rated_indices) == 0:
-        #     raise ValueError("None of the rated movie IDS were found")
-        # Learning a new user vector U_i (eq.2 from instructions)
+        # Learning a new user vector U_i (using eq.2 from the instruction pdf)
         A = V_rated.T @ V_rated + self.lambd * np.eye(self.K)
         b = V_rated.T @ ratings
         U_i = np.linalg.solve(A,b)
         return U_i
+    
 
-# In the active learning loop, a prediction is made forn unrated ,ovies and the 
-# top predicted movie is selected. The user is asked to rate it, and the vector is 
-# once again updated. 
-def active_learning_loop(initial_user_vector, V, R, user_ratings,  max_rounds = 3):
+def cosine_similarity(a,b):
+    return np.dot(a,b) / (norm(a) * norm(b)) if norm(a) > 0 and norm(b) > 0 else 0
+
+def explain_impact(current_user_ratings, movie_to_rate_id, V, R, movieId_to_title, top_k=1, feature_dict = feature_dict, feature_information = feature_characteristics):
+    movie_index = list(R.columns).index(movie_to_rate_id)
+    movie_vector = V[movie_index]
+
+    for hypothetic_rating in range(1,6):
+        simulated_ratings = current_user_ratings.copy()
+        simulated_ratings[movie_to_rate_id] = hypothetic_rating
+
+        cold_start = ColdStart(V,R)
+        updated_user_vector = cold_start.update_user_vector(simulated_ratings)
+        predicted_ratings = V @ updated_user_vector
+        predicted_ratings = np.clip(predicted_ratings, 0, 5)
+
+        rated_ids = set(simulated_ratings.keys())
+        unrated_indices = [idx for idx, movie_id in enumerate(R.columns) if movie_id not in rated_ids]
+
+        top_index = max(unrated_indices, key=lambda i: predicted_ratings[i])
+        top_movie_id = R.columns[top_index]
+        top_movie_title = movieId_to_title.get(top_movie_id, "Unknown")
+        top_movie_vector = V[top_index]
+
+        feature_deltas = updated_user_vector - ColdStart(V,R).update_user_vector(current_user_ratings)
+        top_feature_indices = np.argsort(np.abs(feature_deltas))[::-1][:3]
+        feature_explanations = []
+
+        for idx in top_feature_indices:
+            direction = "increased" if feature_deltas[idx] > 0 else "decreased"
+            user_val = updated_user_vector[idx]
+            movie_val = top_movie_vector[idx]
+            match = "aligns well" if np.sign(feature_deltas[idx]) == np.sign(movie_val) else "differs"
+
+            feature_title = feature_dict['Feature_' + str(idx+1)]
+            feature_info = feature_characteristics['Feature_' + str(idx+1)]
+
+            feature_explanations.append(
+                f"The feature '{feature_title}' {direction} in your profile, and this movie {match} with that change (score: {movie_val:.2f})"
+            )
+
+        print(f"→ If you rate it a {hypothetic_rating}:")
+        print(f"  Next recommended movie: '{top_movie_title}' (ID: {top_movie_id})")
+        print("  Why:")
+        for explanation in feature_explanations:
+            print(f"   - {explanation}")
+        print()
+
+def active_learning_loop(initial_user_vector, V, R, user_ratings, max_rounds=3):
     U = initial_user_vector
     rated_ids = set(user_ratings.keys())
-
     for i in range(max_rounds):
         predicted_ratings = V @ U # Predicted ratings for all movies 
         
@@ -90,44 +124,28 @@ def active_learning_loop(initial_user_vector, V, R, user_ratings,  max_rounds = 
         top_index = max(unrated_indices, key=lambda i:predicted_ratings[i]) 
         top_movie_id = R.columns[top_index] 
         top_movie_title = movieId_to_title.get(top_movie_id, "Unknown title")
-
+        
         print(f" Round {i+1}: Recommended movie: '{top_movie_title}' (movieID: {top_movie_id}) with predicted rating: {predicted_ratings[top_index]:.3f}")
-
-        # TODO: Replace this by the real interface-suitable code. 
+        print(f"Let's see how your ratings will affect your next recommendation:")
+        explain_impact(user_ratings, top_movie_id, V, R, movieId_to_title)
         simulated_rating = random.choice([1,2,3,4,5])
         user_ratings[top_movie_id] = simulated_rating
-        
-        rated_ids.add(top_movie_id) # Marking the movie as rated 
-
-        cold_start = ColdStart(V,R) # Update the user vector 
+        rated_ids.add(top_movie_id)
+        cold_start = ColdStart(V,R)
         U = cold_start.update_user_vector(user_ratings)
 
-    
     return U
 
 
-def explain_impact(current_user_ratings, movie_to_rate_id, V, R, movieId_to_title, top_k=1):
-
-    explainations = {}
-    for rating in range(1,6):
-        pass
-
-
 model, R, U, V = get_R_U_V()
-#print("Movie IDS:",movie_ids)
-# print("R shape:", R.shape)
-# print("V shape:", V.shape)
-# print("U shape:", U.shape)
-
-# print(type(R))               
-# print(R.columns[:20])       
-# print(R.columns.dtype) 
-
-
 
 user_ratings = get_initial_ratings()
+
 cold_start_recommender = ColdStart(V, R)
 user_vector = cold_start_recommender.update_user_vector(user_ratings)
+
 final_user_vector = active_learning_loop(user_vector, V, R, user_ratings, max_rounds=3)
+for i, value in enumerate(final_user_vector):
+    print(f" Feature {feature_dict['Feature_' + str(i+1)]}: {value:2f}")
 
 print(f"Final user vector:", final_user_vector)
---
+++ project_4/Cold_start_recommendation/feature_interpretations.py
@@ -0,0 +1,152 @@
+import numpy as np
+import pandas as pd
+import os
+from collections import Counter
+import matplotlib.pyplot as plt
+from Factorization_engine import get_R_U_V 
+
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+
+csv_path_movies = os.path.join(BASE_DIR, 'data', 'ml_latest_small', 'movies.csv')
+df_movies = pd.read_csv(csv_path_movies)
+movieId_to_title = dict(zip(df_movies['movieId'], df_movies['title']))
+
+# Contextualization of the features. 
+# Purpose: To provide the user with actual, interpretable information about how their choices will affect
+# the next movie to be presented. 
+def interpret_latent_features(V, df_movies, movieId_to_title, top_n=20):
+    feature_interpretations = []
+
+    movie_ids = list(movieId_to_title.keys())
+    movie_index_to_ids = {i: movie_ids[i] for i in range(len(movie_ids))}
+    df_movies = df_movies.set_index('movieId')
+    num_features = V.shape[1]
+
+    for feature_index in range(num_features):
+        top_indices = np.argsort(V[:, feature_index])[::-1][:top_n]
+        top_movie_ids = [movie_index_to_ids[i] for i in top_indices]
+        top_titles = [movieId_to_title[i] for i in top_movie_ids]
+
+        top_genres = []
+        for i in top_movie_ids:
+            if i in df_movies.index:
+                genres = df_movies.loc[i, "genres"]
+                if isinstance(genres, str):
+                    top_genres.extend(genres.split('|'))
+
+        genre_couts = Counter(top_genres)
+
+        summary = {
+            'Feature': feature_index +1, 
+            'Top Genres': genre_couts.most_common(5),
+            'Top Movies': top_titles[:5]
+        }
+
+        feature_interpretations.append(summary)
+
+    return feature_interpretations
+
+
+model, R, U, V = get_R_U_V()
+
+feature_information = interpret_latent_features(V, df_movies, movieId_to_title)
+for feature in feature_information:
+    print(f" Feature {feature['Feature']}")
+    print(" Top Genres:", feature['Top Genres'])
+    print(" Example Movies:", feature['Top Movies'])
+    print()
+
+
+feature_dict = {
+ "Feature_1" : "Offbeat Capers & Musical Mischief",
+ "Feature_2": "Sci-Fi Romance with a Witty Edge", 
+ "Feature_3": "Whimsical Romances & Daydream Escapes",
+ "Feature_4" : "Passion & Peril in Tumultuous Times", 
+ "Feature_5": "Twisted Humor & Creepy Vibes", 
+ "Feature_6": "Poetic Quests & Fantastical Journeys", 
+ "Feature_7" : "Cosmic Comedy & Social Satire",
+ "Feature_8": " Intense Romances & Rebellious Thrills", 
+ "Feature_9": "Low-Key Action & Quiet Intrigue",
+ "Feature_10" : "Classic Suspense with a Romantic Core", 
+ "Feature_11": "Whimsical Worlds for All Ages"
+ }
+
+feature_characteristics = {
+ "Feature_1" : "Quirky Ccrime, satirical comedy, and occasional musical numbers",
+ "Feature_2": "Lighthearted or emotional stories mixing love, tech, and humor", 
+ "Feature_3": "Dreamu love stories, gentre thrillers, and magical realism",
+ "Feature_4" : "Love, war, and danger collide in high-stakes drama", 
+ "Feature_5": "Where horror meets irony -- funny, freaku, and off-kilter", 
+ "Feature_6": "Emotionally rich adventures with surreal or mysitcal tones", 
+ "Feature_7" : "Sci-fi and satire with a splash of existential humor",
+ "Feature_8": "Stylized love stories, edgy drama, and emotional heat", 
+ "Feature_9": "Tense relationships and subtle thrillers with emotional depth",
+ "Feature_10" : "Mysteries and thrillers grounded in emotional stakes", 
+ "Feature_11": "Lighthearted, imaginative stories for families and dreamers."
+ }
+
+
+# Feature 1: Drama (10), Comedy (6), Crime (3), Musical (3), Horror (3)
+# "Quirky Crime & Musical Escapades"
+# Vibe: Stylized, offbeat stories with a blend of humor, music and crime. 
+# Example Movies: Dr.Horrible's Sing-Along Blog, Merry Madagascar, Three Colors: Red
+
+
+# Feature 2: Drama (10), Comedy(8), Sci-Fi (5), Romance (4), Horrror (3)
+# "Sci-Fi Romance with a comedic twist"
+# Vibe: Romantic and humourous films, often with fantastical or science fiction elements
+# Example Movies: Grabbers, Gone with the Wind, Shakespear in Love
+
+
+# Feature 3: Drama (10), Comedy (8), Romance (7), Thriller (3), Fantasy (3)
+# "Dreamy Rom-Coms with a Touch of Magic"
+# Vibe: Romantic fantasies and emotionally risch stories with surreal or magical overtones
+# Example Movies: Charade, Fantastia, Beature of the Day
+
+
+# Feature 4: Drama (12), Romance (5), Thriller (5), War (3), Thriller (4)
+# "Love in Times of War & Tension"
+# Vibe: High-stakes romantic and war-time dramas with intense emotional or politcal themes
+# Example movie: From here to eterity, Universal Soldier, Sleepy Hollow
+
+
+# Feature 5: Drama (12), Comedy (8), Horror (5), Romance (4), Thriller (4)
+# "Dark Comedy Meets Horror"
+# Vibe: Horror and thriller elements combines with humor and romance -- often edgy or ironic
+# Example Movies: Creepshow 2, Arachnophobia, Curly Sue
+
+
+# Feature 6: Drama (10), Comedy (5), Adventure (5), Fantasy (4), Action (4)
+# "Fantastical Adventures with Heart"
+# Vibe: Surreal, emotionally-charged adventures, often poetic or philosophical
+# Example Movies: Dreams, In the Name of the Father, My Name is Bruce
+
+
+# Feature 7: Drama (12), Comedy (9), Adventure (4), Crime (4), Sci-Fi (3)
+# "Sci-Fi & Social Commentary with Laughs"
+# Vibe: Introspective or socially aware films that mix science fiction with humor and satire"
+# Example movies: 2001: A Space Adyssey, Big Chill, 99 Francs
+
+
+# Feature 8: Drama (14), Comedy (6), Romance (5), Thriller (3), Action (3)
+# "Edge Romance & Stylized Thrillers"
+# Vibe: Intimate relationships, rebellious characters, and dramatic twists
+# Example Movies: Before Sunrise, Clockwork Orange, His Girl Friday
+
+
+# Feature 9: Drama (11), Thriller (4), Comedy (4), Romance (4), Action (2)
+# "Mystery & Relaationships on the edge"
+# Vibe: Character-driven stories that blend romance or huor with suspense and danger
+# Example Movie: Raiders of the Lost Ark, Trust, Seven Days in May
+
+
+# Feature 10: Drama (11), Thriller (6), Romance (5), Mystery (5), Comedy (4)
+# "Emotional Thrillers & Classic Myseteries"
+# Vibe: Tense and insprospective stories witgh love, secrets, and string plot twists. 
+# Example Movies: Bound, Out of the Past, Jean de Florette.
+
+
+# Feature 11: Drama (8), Comedy (7), Fantasy (5), adventure (4), Children (4)
+# "Whimsical Family-Friendly Fantasies"
+# Vibe: Light-hearted, often fantastical or satirical films that span generations
+# Example Movies: Wallace and Gromit, Dr.No, Borat, Shakespeare in Love. 
